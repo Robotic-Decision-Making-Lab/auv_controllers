8ecdec84bcf5e45463f993cafacb120b7a09a1fa
Evan Palmer <evanp922@gmail.com>
2025-04-14 22:19:21 -0700

    added angular velocity thruster controller

--- thruster_controllers/src/rotation_rate_controller.cpp
+++ thruster_controllers/src/rotation_rate_controller.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024, Evan Palmer
+// Copyright 2025, Evan Palmer
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "thruster_controllers/polynomial_thrust_curve_controller.hpp"
+#include "thruster_controllers/rotation_rate_controller.hpp"
 
 #include <algorithm>
 #include <cmath>
@@ -34,25 +34,26 @@ namespace thruster_controllers
 namespace
 {
 
-[[nodiscard]] auto calculate_pwm_from_thrust_curve(double force, const std::vector<double> & coefficients) -> int
+[[nodiscard]] auto calculate_angular_velocity_from_thrust(
+  double thrust,
+  double fluid_density,
+  double propeller_diameter,
+  double thrust_coefficient) -> double
 {
-  double pwm = 0.0;
-  for (auto [i, coeff] : std::views::enumerate(coefficients)) {
-    pwm += coeff * std::pow(force, i);
-  }
-  return static_cast<int>(std::round(pwm));
+  const double ang_vel = sqrt(abs(thrust / (fluid_density * thrust_coefficient * pow(propeller_diameter, 4))));
+  return thrust < 0 ? -ang_vel : ang_vel;
 }
 
 }  // namespace
 
-auto PolynomialThrustCurveController::on_init() -> controller_interface::CallbackReturn
+auto RotationRateController::on_init() -> controller_interface::CallbackReturn
 {
-  param_listener_ = std::make_shared<polynomial_thrust_curve_controller::ParamListener>(get_node());
+  param_listener_ = std::make_shared<rotation_rate_controller::ParamListener>(get_node());
   params_ = param_listener_->get_params();
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-auto PolynomialThrustCurveController::update_parameters() -> void  // NOLINT
+auto RotationRateController::update_parameters() -> void  // NOLINT
 {
   if (!param_listener_->is_old(params_)) {
     return;
@@ -61,14 +62,14 @@ auto PolynomialThrustCurveController::update_parameters() -> void  // NOLINT
   params_ = param_listener_->get_params();
 }
 
-auto PolynomialThrustCurveController::configure_parameters() -> controller_interface::CallbackReturn
+auto RotationRateController::configure_parameters() -> controller_interface::CallbackReturn
 {
   update_parameters();
   thruster_name_ = params_.thruster;
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-auto PolynomialThrustCurveController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+auto RotationRateController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
   -> controller_interface::CallbackReturn
 {
   configure_parameters();
@@ -95,7 +96,7 @@ auto PolynomialThrustCurveController::on_configure(const rclcpp_lifecycle::State
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-auto PolynomialThrustCurveController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+auto RotationRateController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
   -> controller_interface::CallbackReturn
 {
   reference_.readFromNonRT()->data = std::numeric_limits<double>::quiet_NaN();
@@ -103,41 +104,37 @@ auto PolynomialThrustCurveController::on_activate(const rclcpp_lifecycle::State 
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-auto PolynomialThrustCurveController::command_interface_configuration() const
-  -> controller_interface::InterfaceConfiguration
+auto RotationRateController::command_interface_configuration() const -> controller_interface::InterfaceConfiguration
 {
   controller_interface::InterfaceConfiguration command_interface_config;
   command_interface_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
   command_interface_config.names.reserve(1);
-  command_interface_config.names.emplace_back(std::format("{}/pwm", thruster_name_));
+  command_interface_config.names.emplace_back(std::format("{}/{}", thruster_name_, hardware_interface::HW_IF_VELOCITY));
   return command_interface_config;
 }
 
-auto PolynomialThrustCurveController::state_interface_configuration() const
-  -> controller_interface::InterfaceConfiguration
+auto RotationRateController::state_interface_configuration() const -> controller_interface::InterfaceConfiguration
 {
   controller_interface::InterfaceConfiguration state_interface_config;
   state_interface_config.type = controller_interface::interface_configuration_type::NONE;
   return state_interface_config;
 }
 
-auto PolynomialThrustCurveController::on_export_reference_interfaces()
-  -> std::vector<hardware_interface::CommandInterface>
+auto RotationRateController::on_export_reference_interfaces() -> std::vector<hardware_interface::CommandInterface>
 {
   reference_interfaces_.resize(1, std::numeric_limits<double>::quiet_NaN());
-
   std::vector<hardware_interface::CommandInterface> interfaces;
   interfaces.reserve(reference_interfaces_.size());
 
   interfaces.emplace_back(
     get_node()->get_name(),
     std::format("{}/{}", thruster_name_, hardware_interface::HW_IF_EFFORT),
-    &reference_interfaces_[0]);
+    &reference_interfaces_[0]);  // NOLINT(readability-container-data-pointer)
 
   return interfaces;
 }
 
-auto PolynomialThrustCurveController::update_reference_from_subscribers(
+auto RotationRateController::update_reference_from_subscribers(
   const rclcpp::Time & /*time*/,
   const rclcpp::Duration & /*period*/) -> controller_interface::return_type
 {
@@ -147,24 +144,23 @@ auto PolynomialThrustCurveController::update_reference_from_subscribers(
   return controller_interface::return_type::OK;
 }
 
-auto PolynomialThrustCurveController::update_and_write_commands(
-  const rclcpp::Time & time,
-  const rclcpp::Duration & period) -> controller_interface::return_type
+auto RotationRateController::update_and_write_commands(const rclcpp::Time & time, const rclcpp::Duration & period)
+  -> controller_interface::return_type
 {
-  const auto reference = reference_interfaces_[0];
+  double reference = reference_interfaces_[0];
 
   if (std::isnan(reference)) {
     if (!command_interfaces_[0].set_value(reference)) {
-      RCLCPP_WARN(  // NOLINT
-        get_node()->get_logger(),
-        std::format("Failed to set command for thruster {}", thruster_name_).c_str());
+      RCLCPP_WARN(
+        get_node()->get_logger(), std::format("Failed to set command for thruster {}", thruster_name_).c_str());
     }
   } else {
-    const double clamped_reference = std::clamp(reference, params_.min_thrust, params_.max_thrust);
-    int pwm = calculate_pwm_from_thrust_curve(clamped_reference, params_.thrust_curve_coefficients);
-    pwm = pwm > params_.min_deadband_pwm && pwm < params_.max_deadband_pwm ? params_.neutral_pwm : pwm;
+    reference = std::clamp(reference, params_.min_thrust, params_.max_thrust);
+    reference = reference < params_.max_deadband && reference > params_.min_deadband ? 0.0 : reference;
+    const double ang_vel = calculate_angular_velocity_from_thrust(
+      reference, params_.fluid_density, params_.propeller_diameter, params_.thrust_coefficient);
 
-    if (!command_interfaces_[0].set_value(pwm)) {
+    if (!command_interfaces_[0].set_value(ang_vel)) {
       RCLCPP_WARN(  // NOLINT
         get_node()->get_logger(),
         std::format("Failed to set command for thruster {}", thruster_name_).c_str());
@@ -192,6 +188,4 @@ auto PolynomialThrustCurveController::update_and_write_commands(
 
 }  // namespace thruster_controllers
 
-PLUGINLIB_EXPORT_CLASS(
-  thruster_controllers::PolynomialThrustCurveController,
-  controller_interface::ChainableControllerInterface)
+PLUGINLIB_EXPORT_CLASS(thruster_controllers::RotationRateController, controller_interface::ChainableControllerInterface)
